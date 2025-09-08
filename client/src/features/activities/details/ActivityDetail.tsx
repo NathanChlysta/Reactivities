ec10f5010c46099064a3c635f15d4197f70e8a85
NathanChlysta <chlystan@gmail.com>
2025-09-08 12:28:43 -0400

    End of Section 7. Routing.

--- client/src/features/activities/details/ActivityDetail.tsx
+++ client/src/features/activities/details/ActivityDetail.tsx
@@ -1,33 +1,37 @@
 import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material"
-import { useActivities } from "../../../lib/hooks/useActivities"
+import { useParams } from "react-router";
+import { Link } from "react-router";
+import { useNavigate } from "react-router";
+import { useActivities } from "../../../lib/hooks/useActivities";
 
-type Props = {
-    selectedActivity: Activity
-    cancelSelectActivity: () => void
-    openForm: (id: string) => void
-}
 
-export default function ActivityDetail({selectedActivity, cancelSelectActivity, openForm}: Props) {
-const {activities} = useActivities();
-const activity = activities?.find(x => x.id === selectedActivity.id);
+export default function ActivityDetail() {
+    const navigate = useNavigate();
+    const {id} = useParams();
+    const {activity, isLoadingActivity} = useActivities(id);
 
-if (!activity) return <Typography>Loading....</Typography>
+    if (isLoadingActivity) return <Typography>Loading...</Typography>
 
-  return (
-    <Card sx={{borderRadius: 3}}>
-        <CardMedia
-            component='img'
-            src={`/images/categoryImages/${activity.category}.jpg`}
-        />
-        <CardContent>
-            <Typography variant="h5">{activity.title}</Typography>
-            <Typography variant="subtitle1" fontWeight='light'>{activity.date}</Typography>
-            <Typography variant="body1">{activity.description}</Typography>
-        </CardContent>
-        <CardActions>
-            <Button onClick={() => openForm(activity.id)} color="primary">Edit</Button>
-            <Button onClick={cancelSelectActivity} color="inherit">Cancel</Button>
-        </CardActions>
-    </Card>
-  )
+    if (!activity) return <Typography>Activity Not Found</Typography>
+
+
+    if (!activity) return <Typography>Loading....</Typography>
+
+    return (
+        <Card sx={{ borderRadius: 3 }}>
+            <CardMedia
+                component='img'
+                src={`/images/categoryImages/${activity.category}.jpg`}
+            />
+            <CardContent>
+                <Typography variant="h5">{activity.title}</Typography>
+                <Typography variant="subtitle1" fontWeight='light'>{activity.date}</Typography>
+                <Typography variant="body1">{activity.description}</Typography>
+            </CardContent>
+            <CardActions> 
+                <Button component={Link} to={`/manage/${activity.id}`} color="primary">Edit</Button>
+                <Button onClick={() => navigate('/activities')} color="inherit">Cancel</Button>
+            </CardActions>
+        </Card>
+    )
 }
